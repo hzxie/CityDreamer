178b5b4128aeddfbf307bf08bbdba7e0e99b3f96
Haozhe Xie <root@haozhexie.com>
2023-04-15 10:45:36 +0800

    Rename cu_extrude_tensor to extrude_tensor.

--- extensions/extrude_tensor/setup.py
+++ extensions/extrude_tensor/setup.py
@@ -4,14 +4,14 @@
 # @Author: Haozhe Xie
 # @Date:   2023-03-24 20:35:43
 # @Last Modified by: Haozhe Xie
-# @Last Modified at: 2023-03-26 15:40:52
+# @Last Modified at: 2023-04-15 10:37:12
 # @Email:  root@haozhexie.com
 
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 setup(
-    name="cu_extrude_tensor",
+    name="extrude_tensor",
     version="1.0.0",
     ext_modules=[
         CUDAExtension(
