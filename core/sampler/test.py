hzxie/CityDreamer

commit 34ec5bd6109753bd7c2b1d4fc7dc224904de654d

Make tensor index more efficient.

core/sampler/test.py
@@ -4,7 +4,7 @@
 # @Author: Haozhe Xie
 # @Date:   2023-04-10 10:46:40
 # @Last Modified by: Haozhe Xie
-# @Last Modified at: 2023-04-14 10:39:02
+# @Last Modified at: 2023-04-18 15:49:55
 # @Email:  root@haozhexie.com
 
 import logging
@@ -82,9 +82,9 @@ def test(cfg, vqae=None, sampler=None):
                 "HeightField",
             )
             key_frames["Image/T=%d/FootprintCtr" % t] = utils.helpers.tensor_to_image(
-                torch.cat(
-                    [torch.sigmoid(pred[0, 1]), torch.sigmoid(pred[1, 1])], dim=1
-                ).unsqueeze(dim=0),
+                torch.cat([torch.sigmoid(pred[0, 1]), pred[1, 1]], dim=1).unsqueeze(
+                    dim=0
+                ),
                 "FootprintCtr",
             )
             key_frames["Image/T=%d/SegMap" % t] = utils.helpers.tensor_to_image(

core/vqgan/test.py
@@ -4,7 +4,7 @@
 # @Author: Haozhe Xie
 # @Date:   2023-04-06 09:50:44
 # @Last Modified by: Haozhe Xie
-# @Last Modified at: 2023-04-14 10:38:58
+# @Last Modified at: 2023-04-18 15:49:57
 # @Email:  root@haozhexie.com
 
 import logging
@@ -77,10 +77,10 @@ def test(cfg, test_data_loader=None, vqae=None):
             )
 
             key_frames["Image/%04d/HeightField" % idx] = utils.helpers.tensor_to_image(
-                torch.cat([pred[:, 0], output[:, 0]], dim=2), "HeightField"
+                torch.cat([pred[:, [0]], output[:, [0]]], dim=3), "HeightField"
             )
             key_frames["Image/%04d/FootprintCtr" % idx] = utils.helpers.tensor_to_image(
-                torch.cat([torch.sigmoid(pred[:, 1]), torch.sigmoid(output[:, 1])], dim=2),
+                torch.cat([torch.sigmoid(pred[:, [1]]), output[:, [1]]], dim=3),
                 "FootprintCtr",
             )
             key_frames["Image/%04d/SegMap" % idx] = utils.helpers.tensor_to_image(

commit 78b8048c718fbe9ce216cb1cb4996a7457ca1297

Fix stupid bugs introduced in commit 34ec5bd610.

core/sampler/test.py
@@ -4,7 +4,7 @@
 # @Author: Haozhe Xie
 # @Date:   2023-04-10 10:46:40
 # @Last Modified by: Haozhe Xie
-# @Last Modified at: 2023-04-18 15:49:55
+# @Last Modified at: 2023-04-18 21:50:30
 # @Email:  root@haozhexie.com
 
 import logging
@@ -78,24 +78,22 @@ def test(cfg, vqae=None, sampler=None):
             # print(quant.size())   # torch.Size([bs, embed_dim, att_size, att_size])
             pred = vqae.module.decode(quant)
             key_frames["Image/T=%d/HeightField" % t] = utils.helpers.tensor_to_image(
-                torch.cat([pred[0, 0], pred[1, 0]], dim=1).unsqueeze(dim=0),
+                torch.cat([pred[[0], 0], pred[[1], 0]], dim=2),
                 "HeightField",
             )
             key_frames["Image/T=%d/FootprintCtr" % t] = utils.helpers.tensor_to_image(
-                torch.cat([torch.sigmoid(pred[0, 1]), pred[1, 1]], dim=1).unsqueeze(
-                    dim=0
-                ),
+                torch.cat([torch.sigmoid(pred[[0], 1]), pred[[1], 1]], dim=2),
                 "FootprintCtr",
             )
             key_frames["Image/T=%d/SegMap" % t] = utils.helpers.tensor_to_image(
                 utils.helpers.onehot_to_mask(
                     torch.cat(
                         [
-                            pred[0, 2:],
-                            pred[1, 2:],
+                            pred[[0], 2:],
+                            pred[[1], 2:],
                         ],
-                        dim=2,
-                    ).unsqueeze(dim=0),
+                        dim=3,
+                    ),
                     cfg.DATASETS.OSM_LAYOUT.IGNORED_CLASSES,
                 ),
                 "SegMap",

core/vqgan/test.py
@@ -4,7 +4,7 @@
 # @Author: Haozhe Xie
 # @Date:   2023-04-06 09:50:44
 # @Last Modified by: Haozhe Xie
-# @Last Modified at: 2023-04-18 15:49:57
+# @Last Modified at: 2023-04-18 22:06:55
 # @Email:  root@haozhexie.com
 
 import logging
@@ -77,10 +77,10 @@ def test(cfg, test_data_loader=None, vqae=None):
             )
 
             key_frames["Image/%04d/HeightField" % idx] = utils.helpers.tensor_to_image(
-                torch.cat([pred[:, [0]], output[:, [0]]], dim=3), "HeightField"
+                torch.cat([pred[:, 0], output[:, 0]], dim=2), "HeightField"
             )
             key_frames["Image/%04d/FootprintCtr" % idx] = utils.helpers.tensor_to_image(
-                torch.cat([torch.sigmoid(pred[:, [1]]), output[:, [1]]], dim=3),
+                torch.cat([torch.sigmoid(pred[:, 1]), output[:, 1]], dim=2),
                 "FootprintCtr",
             )
             key_frames["Image/%04d/SegMap" % idx] = utils.helpers.tensor_to_image(

utils/helpers.py
@@ -4,7 +4,7 @@
 # @Author: Haozhe Xie
 # @Date:   2023-04-06 10:25:10
 # @Last Modified by: Haozhe Xie
-# @Last Modified at: 2023-04-14 10:10:00
+# @Last Modified at: 2023-04-18 22:10:38
 # @Email:  root@haozhexie.com
 
 import numpy as np
@@ -69,11 +69,11 @@ def onehot_to_mask(onehot, ignored_classes=[]):
 
 def tensor_to_image(tensor, mode):
     # assert mode in ["HeightField", "FootprintCtr", "SegMap"]
-    tensor = tensor.permute(1, 2, 0).cpu().numpy()
+    tensor = tensor.cpu().numpy()
     if mode == "HeightField":
-        return tensor.squeeze() / np.max(tensor)
+        return tensor.transpose((1, 2, 0)).squeeze() / np.max(tensor)
     elif mode == "FootprintCtr":
-        return tensor.squeeze()
+        return tensor.transpose((1, 2, 0)).squeeze()
     elif mode == "SegMap":
         return get_seg_map(tensor.squeeze()).convert("RGB")
     else: